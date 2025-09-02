2fa453191c7c631542ea876f179d57bf7e6abf3c
elviredev <sandrine.rodriguez@hotmail.com>
2025-09-02 15:30:37 +0200

    🐶 Add Admin dashboard (Header, Sidebar, Footer), Login (2FA vérification de code), Register, Logout.

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerificationCodeMail;
+use Random\RandomException;
+
+class AdminController extends Controller
+{
+  /**
+   * @desc Destroy an authenticated session.
+   * @param Request $request
+   * @return RedirectResponse
+   */
+  public function adminLogout(Request $request): RedirectResponse
+  {
+    Auth::guard('web')->logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect('/login');
+  }
+
+  /**
+   * @desc login with TwoFactor Authentication (2FA) using a verification code
+   * @param Request $request
+   * @return RedirectResponse
+   * @throws RandomException
+   */
+  public function adminLogin(Request $request)
+  {
+    $credentials = $request->only('email', 'password');
+
+    // Si user connecté
+    if(Auth::attempt($credentials)) {
+      $user = Auth::user();
+
+      $verificationCode = random_int(100000, 999999);
+
+      session([
+        'verification_code' => $verificationCode,
+        'user_id' => $user->id,
+      ]);
+
+      // envoi du code à l'utilisateur connecté
+      Mail::to($user->email)->send(new VerificationCodeMail($verificationCode));
+
+      Auth::logout();
+
+      // Redirect vers le formulaire de vérification du code
+      return redirect()->route('custom.verification.form')
+                       ->with('success', 'Verification code has been sent to your email.');
+    }
+
+    return redirect()->back()->withErrors(['email' => 'Invalid credentials provided.']);
+  }
+
+  /**
+   * @desc Formulaire de vérification du code Two-Factor Authentication (2FA)
+   * @return Factory|View|\Illuminate\View\View
+   */
+  public function showVerification()
+  {
+    return view('auth.verify');
+  }
+
+  /**
+   * @desc Vérification du code 2FA Authentification
+   * @param Request $request
+   * @return RedirectResponse
+   */
+  public function verificationVerify(Request $request)
+  {
+    $request->validate(['code' => 'required|numeric']);
+
+    if ($request->code == session('verification_code')) {
+      Auth::loginUsingId(session('user_id'));
+
+      session()->forget(['verification_code', 'user_id']);
+
+      return redirect()->intended('/dashboard');
+    }
+
+    return back()->withErrors(['code' => 'Invalid verification code.']);
+  }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/admin/logout', [AdminController::class, 'adminLogout'])->name('admin.logout');
+Route::post('/admin/login', [AdminController::class, 'adminLogin'])->name('admin.login');
+Route::get('/verify', [AdminController::class, 'showVerification'])->name('custom.verification.form');
+Route::post('/verify', [AdminController::class, 'verificationVerify'])->name('custom.verification.verify');
